e60b397795278d1c04e5d77a7fc4e1e4b78b2e81
Qingkun Deng <106399042+Ericdqk001@users.noreply.github.com>
2025-06-23 18:28:23 +0100

    Complete the train pipeline

--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
 from src.LCA.lca_cbcl import perform_lca
+from src.modelling.train.train import train
 from src.preprocess.deconfound import deconfound
 from src.preprocess.prepare_data import prepare_data
 from src.preprocess.split import split
 
 
-def main(version_name: str = "test"):
+def main(
+    version_name: str = "test",
+    num_blrt_repetitions: int = 1,
+):
     # Prepare the data for the specified wave
     prepare_data(version_name=version_name)
 
     # Perform LCA on the prepared data
     perform_lca(
         version_name=version_name,
-        num_blrt_repetitions=1,
+        num_blrt_repetitions=num_blrt_repetitions,
     )
 
     # Split the data into sets
@@ -24,9 +28,54 @@ def main(version_name: str = "test"):
         version_name=version_name,
     )
 
+    modality_list = [
+        "cortical_thickness",
+        "cortical_surface_area",
+        "cortical_volume",
+        "subcortical_volume",
+    ]
+
+    for modality in modality_list:
+        modality_train_config = modality_train_confit[modality]
+        modality_train_config["modality"] = modality
+        train(modality_train_config)
+
 
 if __name__ == "__main__":
+    modality_train_confit = {
+        "cortical_thickness": {
+            "hidden_dim": [10],
+            "latent_dim": 5,
+            "batch_size": 64,
+            "learning_rate": 0.01,
+            "epochs": 10,
+        },
+        "cortical_surface_area": {
+            "hidden_dim": [10],
+            "latent_dim": 5,
+            "batch_size": 64,
+            "learning_rate": 0.01,
+            "epochs": 10,
+        },
+        "cortical_volume": {
+            "hidden_dim": [10],
+            "latent_dim": 5,
+            "batch_size": 64,
+            "learning_rate": 0.01,
+            "epochs": 10,
+        },
+        "subcortical_volume": {
+            "hidden_dim": [10],
+            "latent_dim": 5,
+            "batch_size": 64,
+            "learning_rate": 0.01,
+            "epochs": 10,
+        },
+    }
+
     version_name = "test"
+    num_blrt_repetitions = 1
     main(
         version_name=version_name,
+        num_blrt_repetitions=num_blrt_repetitions,
     )
--- src/modelling/train/train.py
+++ src/modelling/train/train.py
@@ -1,127 +1,90 @@
 import json
-from argparse import ArgumentParser
+import logging
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 import torch
-from modelling.load.load import MyDataset
-from modelling.models.VAE import VAE
 from sklearn.preprocessing import StandardScaler
 from torch.utils.data import DataLoader
 
-import wandb
+from src.modelling.load.load import MyDataset
+from src.modelling.models.VAE import VAE
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-parser = ArgumentParser(description="Tune cVAE")
+version_name = "test"
+# %%
+logging.info("-----------------------")
+logging.info("Training VAE model")
 
-
-# python src/modelling/train/VAE/VAE_train.py --data_path "data/processed_data" --feature_type "rsfmri" --project_title "VAE_rsfmri_final_train" --batch_size 256 --learning_rate 0.005 --latent_dim 13 --hidden_dim "30"
-
-
-def int_parse_list(arg_value):
-    return [int(x) for x in arg_value.split("-")]
-
-
-parser.add_argument(
-    "--data_path",
-    type=str,
-    default="",
-    help="Path to processed data",
-)
-parser.add_argument(
-    "--feature_type",
-    type=str,
-    default="",
-    help="Brain features of interest, options: 'cortical_thickness, cortical_volume, rsfmri'",
-)
-parser.add_argument(
-    "--project_title",
-    type=str,
-    default="Tune cVAE",
-    help="Title of the project for weights and biases",
-)
-parser.add_argument(
-    "--batch_size",
-    type=int,
-    default=128,
-    help="Batch size for training, e.g., '64-128-256', which will be parsed into [64, 128, 256]",
-)
-parser.add_argument(
-    "--learning_rate",
-    type=float,
-    default=0.01,
-    help="Learning rate for the optimizer, e.g., '0.005-0.001-0.0005-0.0001', which will be parsed into [0.005, 0.001, 0.0005, 0.0001]",
-)
-parser.add_argument(
-    "--latent_dim",
-    type=int,
-    default=5,
-    help="Dimensions of the latent space, e.g., '10-11-12', which will be parsed into [10, 11, 12]",
-)
-parser.add_argument(
-    "--hidden_dim",
-    type=int_parse_list,
-    default=[30],
-    help="Pass dimensions of multiple hidden layers, use ';' to separate layers and '-' to separate dimensions within layers, e.g., '30-30;40-40-40'",
+data_store_path = Path(
+    "/",
+    "Volumes",
+    "GenScotDepression",
 )
 
-args = parser.parse_args()
-processed_data_path = Path(args.data_path)
-feature_type = args.feature_type
-
-feature_sets_map = {
-    "cortical_thickness": "t1w_cortical_thickness_rois",
-    "cortical_volume": "t1w_cortical_volume_rois",
-    "rsfmri": "gordon_net_subcor_limbic_no_dup",
-}
+if data_store_path.exists():
+    logging.info("Mounted data store path: %s", data_store_path)
 
 
-brain_features_of_interest_path = Path(
-    processed_data_path,
-    "brain_features_of_interest.json",
+analysis_root_path = Path(
+    data_store_path,
+    "users",
+    "Eric",
+    "nm",
 )
 
-with open(brain_features_of_interest_path, "r") as f:
-    brain_features_of_interest = json.load(f)
-
-FEATURES = brain_features_of_interest[feature_sets_map[feature_type]]
-
-
-TRAIN_DATA_PATH = Path(
-    processed_data_path,
-    "all_brain_features_resid.csv",
+processed_data_path = Path(
+    analysis_root_path,
+    version_name,
+    "processed_data",
 )
 
 CHECKPOINT_PATH = Path(
+    analysis_root_path,
+    version_name,
     "checkpoints",
-    feature_type,
 )
 
 if not CHECKPOINT_PATH.exists():
-    CHECKPOINT_PATH.mkdir(parents=True)
+    CHECKPOINT_PATH.mkdir(parents=True, exist_ok=True)
+
+imaging_features_path = Path(
+    processed_data_path,
+    "mri_all_features_post_deconfound.csv",
+)
+
+DATA = pd.read_csv(
+    imaging_features_path,
+    index_col=0,
+    low_memory=False,
+)
+
+brain_features_of_interest_path = Path(
+    processed_data_path,
+    "features_of_interest.json",
+)
+
+with open(brain_features_of_interest_path, "r") as f:
+    FEATURES_OF_INTEREST = json.load(f)
 
 data_splits_path = Path(
     processed_data_path,
-    "data_splits_with_clinical_val.json",
+    "imaging_data_splits.json",
 )
 
 with open(data_splits_path, "r") as f:
     data_splits = json.load(f)
 
-if "cortical" in feature_type:
-    modality_data_split = data_splits["structural"]
 
-else:
-    modality_data_split = data_splits["functional"]
+TRAIN_SUBS = data_splits["train"]
+VAL_SUBS = data_splits["val"]
 
 
-TRAIN_SUBS = modality_data_split["train"]
-VAL_SUBS = modality_data_split["val"]
-
-
-def build_model(config, input_dim):
+def build_model(
+    config,
+    input_dim,
+):
     # Set random seed for CPU
     torch.manual_seed(123)
 
@@ -138,36 +101,14 @@ def build_model(config, input_dim):
         non_linear=True,
     ).to(DEVICE)
 
-    # if isinstance(model, nn.Module):
-    #     first_layer_params = list(model.parameters())[0]
-    # print("Parameter values of the first layer:", first_layer_params)
-
     return model
 
 
-def one_hot_encode_covariate(
-    data,
-    covariate,
-    subjects,
+def validate(
+    model,
+    val_loader,
+    device,
 ):
-    """Return one hot encoded covariate for the given subjects as required by the cVAE model."""
-    covariate_data = data.loc[
-        subjects,
-        [covariate],
-    ]
-
-    covariate_data[covariate] = pd.Categorical(covariate_data[covariate])
-
-    category_codes = covariate_data[covariate].cat.codes
-
-    num_categories = len(covariate_data[covariate].cat.categories)
-
-    one_hot_encoded_covariate = np.eye(num_categories)[category_codes]
-
-    return one_hot_encoded_covariate
-
-
-def validate(model, val_loader, device):
     model.eval()
     total_val_loss = 0.0
     total_recon = 0.0
@@ -187,38 +128,37 @@ def validate(model, val_loader, device):
     mean_recon = total_recon / len(val_loader)
     mean_kl_loss = total_kl_loss / len(val_loader)
 
-    wandb.log({"val_total_loss": mean_val_loss})
-    wandb.log({"val_recon": mean_recon})
-    wandb.log({"val_kl_loss": mean_kl_loss})
+    logging.info(
+        "Validation loss: %.4f, Recon loss: %.4f, KL loss: %.4f",
+        mean_val_loss,
+        mean_recon,
+        mean_kl_loss,
+    )
 
     return mean_val_loss
 
 
-def train(
+def train_model(
     config,
     model,
     train_loader,
     val_loader,
     tolerance=50,
 ):
-
     model.to(DEVICE)
 
     best_val_loss = float("inf")
 
-    model_artifact = wandb.Artifact(wandb.run.name, type="model")
-
     epochs_no_improve = 0
 
-    for epoch in range(1, config.epochs + 1):
-
+    for epoch in range(1, config["epochs"] + 1):
         total_loss = 0.0
         total_recon = 0.0
         kl_loss = 0.0
 
         model.train()
 
-        for batch_idx, batch in enumerate(train_loader):
+        for _, batch in enumerate(train_loader):
             data_curr = batch.to(DEVICE)
             fwd_rtn = model.forward(data_curr)
             loss = model.loss_function(data_curr, fwd_rtn)
@@ -236,74 +176,58 @@ def train(
             epochs_no_improve = 0
             best_val_loss = val_loss
 
-            print("New best val loss:", val_loss)
-            print("at epoch:", epoch)
+            logging.info(
+                "Epoch %d: New best val score: %.4f",
+                epoch,
+                best_val_loss,
+            )
 
             best_model = model.state_dict()
-
-            print("Improved at epoch:", epoch)
 
         else:
             epochs_no_improve += 1
 
         if epochs_no_improve >= tolerance:
-            print("Early stopping at epoch:", epoch)
+            logging.info(
+                "Early stopping triggered after %d epochs without improvement.",
+                epochs_no_improve,
+            )
+
+            modality = config["modality"]
 
             torch.save(
                 best_model,
-                Path(CHECKPOINT_PATH, "VAE_model_weights.pt"),
+                Path(CHECKPOINT_PATH, f"VAE_model_weights{modality}.pt"),
             )
-
-            model_artifact.add_file(
-                Path(CHECKPOINT_PATH, "VAE_model_weights.pt"),
-            )
-
-            wandb.run.log_artifact(model_artifact)
 
             break
 
-        wandb.log(
-            {
-                "Epoch_total_loss": total_loss / len(train_loader),
-                "Epoch_recon_loss": total_recon / len(train_loader),
-                "Epoch_kl_loss": kl_loss / len(train_loader),
-            }
+        logging.info(
+            "Epoch %d: Train total loss: %.4f, Recon loss: %.4f, KL loss: %.4f",
+            epoch,
+            total_loss / len(train_loader),
+            total_recon / len(train_loader),
+            kl_loss / len(train_loader),
         )
 
 
-# Remove subjects with high entropy
-# high_entropy_subs_path = Path(
-#     "data",
-#     "LCA",
-#     "subjects_with_high_entropy.csv",
-# )
+def train(config):
+    modality = config["modality"]
 
-# high_entropy_subs = pd.read_csv(
-#     high_entropy_subs_path,
-#     low_memory=False,
-# )["subject"].tolist()
+    logging.info("Training VAE model for modality: %s", modality)
 
-# TRAIN_SUBS = [sub for sub in TRAIN_SUBS if sub not in high_entropy_subs]
+    logging.info("Using coniguration: %s", config)
 
-# VAL_SUBS = [sub for sub in VAL_SUBS if sub not in high_entropy_subs]
+    features = FEATURES_OF_INTEREST[modality]
 
-
-def main(config):
-
-    data = pd.read_csv(
-        Path(TRAIN_DATA_PATH),
-        index_col=0,
-        low_memory=False,
-    )
-
-    train_dataset = data.loc[
+    train_dataset = DATA.loc[
         TRAIN_SUBS,
-        FEATURES,
+        features,
     ].to_numpy()
 
-    val_dataset = data.loc[
+    val_dataset = DATA.loc[
         VAL_SUBS,
-        FEATURES,
+        features,
     ].to_numpy()
 
     scaler = StandardScaler()
@@ -314,13 +238,13 @@ def main(config):
 
     train_loader = DataLoader(
         MyDataset(train_data),
-        batch_size=config.batch_size,
+        batch_size=config["batch_size"],
         shuffle=True,
     )
 
     val_loader = DataLoader(
         MyDataset(val_data),
-        batch_size=config.batch_size,
+        batch_size=config["batch_size"],
         shuffle=False,
     )
 
@@ -331,7 +255,7 @@ def main(config):
         input_dim,
     )
 
-    train(
+    train_model(
         config,
         model,
         train_loader,
@@ -340,21 +264,13 @@ def main(config):
 
 
 if __name__ == "__main__":
+    config = {
+        "modality": "cortical_thickness",
+        "hidden_dim": [10],
+        "latent_dim": 5,
+        "learning_rate": 0.01,
+        "batch_size": 64,
+        "epochs": 10,
+    }
 
-    project_title = args.project_title
-    batch_size = args.batch_size
-    learning_rate = args.learning_rate
-    latent_dim = args.latent_dim
-    hidden_dim = args.hidden_dim
-
-    with wandb.init(
-        project=f"VAE_{feature_type}_final_train",
-        config={
-            "batch_size": batch_size,
-            "hidden_dim": hidden_dim,
-            "latent_dim": latent_dim,
-            "learning_rate": learning_rate,
-            "epochs": 5000,
-        },
-    ):
-        main(wandb.config)
+    train(config)
